30f0a22143ebeabe2e68581113ce1edebdd3d685
Jason J Cruz <jaycru06@yahoo.com>
2019-11-11 19:22:43 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -13,7 +13,7 @@ def current_player(board)
   counter = 0 
   while counter <= board.length - 1 
     counter += 1
-    if board[counter] % 2 == 0
+    if turn_count(board) && counter % 2 == 0
       return "X" 
     else 
       return "O"
